9fccbd660be6fd98bc234f0d53ab223beeac3b40
Matheus A Fagundes <matheusfagundes2020@outlook.com.br>
2023-07-27 20:15:15 -0300

    Adicionando

--- Exercicio-Salarial.py
+++
@@ -1,3 +0,0 @@
-salario = float(input('Qual é o salario do funcionario? R$'))
-novo = salario + (salario * 15 / 100)
-print('Um funcionario que ganhava R${:.2f}, com 15% de aumento, passaa receber R${:.2f}'.format(salario, novo))
